7b2bbd12bb9c5ab117df0d84a8edd77e30c8ceff
Jean-Paul Mari <djipi.mari@gmail.com>
2024-07-24 21:25:26 -0400

    Add width & height args in test bitmap

--- AJaguar/test/src/testbitmap_args.c
+++ AJaguar/test/src/testbitmap_args.c
@@ -6,6 +6,10 @@ char* NXArgv[] = {
 	(char*)"testbitmap.elf",
 	(char*)"-bpp",
 	(char*)"8",
+	(char*)"-width",
+	(char*)"320",
+	(char*)"-height",
+	(char*)"240"
 	// (char*)"-warp",
 	// (char*)"-hw",
 	// (char*)"-fullscreen",
